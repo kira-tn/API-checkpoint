ddfeabac1000e81ca3db8ecd72822f1e80c05955
Majdoub Nidhal <39164676+kira-tn@users.noreply.github.com>
2021-12-01 22:34:55 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import {useState,useEffect} from 'react';
+import axios from 'axios';
+import UserList from './UserList';
+
+function App() {
+
+  const [listOfUSer,setListOfUser] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://jsonplaceholder.typicode.com/users`);
+      setListOfUser(result.data);
+      setIsLoading(false);
+    }
+    fetchItems();
+  },[]);
+
+
+
+
+  return (
+    <div className="App container">
+
+      <div className="row">
+      <UserList listOfUSer={listOfUSer} isLoading={isLoading} />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
